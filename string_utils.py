7d0504ff836c030431e62481bc6b83c8f3f6211b
ellawo0212 <ellawo0212@gmail.com>
2025-12-09 22:36:24 +0200

    Refactor string utility functions for clarity

--- string_utils.py
+++ string_utils.py
@@ -1,45 +1,40 @@
+def split_before_each_uppercases(formula):
+  split_formula = []
+  start = 0
+  end = 1
+  if formula != "":
+    for i in range(1, len(formula)):
+      if formula[i].isupper():
+        end = i
+        split_formula.append(formula[start:end])
+        start = i
+    split_formula.append(formula[start:len(formula)])
+    return split_formula
+  else:
+    return []
 
-
-
-def split_before_uppercases(formula):
-    start = 0
-    end = 0
-    split_formula = []
-    if formula != "":
-      for i in range(1 , len(formula)):
-        end += 1
-        if formula[i].isupper() == True:
-          split_formula.append(formula[start : end])
-          start = end
-      split_formula.append(formula[start:len(formula)] )
-      return split_formula
+def split_at_first_digit(formula):
+  digit_location = 1
+  for i in range(1, len(formula)):
+    if formula[i].isdigit() == True:
+      break
     else:
-       return []
-        
-def split_at_digit(formula):
-    digit_location = 1
-    for i in range(1,len(formula)):
-      if formula[i].isdigit():
-        digit_location = i
-        break
-      else:
-        digit_location += 1
+      digit_location += 1
+  if digit_location == len(formula):
+    return formula, 1
+  else:
+    prefix = formula[:digit_location]
+    numeric = int(formula[digit_location::])
+    return prefix,numeric
 
-    if digit_location == len(formula):
-       return formula , 1 
-    else:
-      prefix = formula[:digit_location]
-      numeric = int(formula[digit_location::])
-      return prefix , numeric
-        
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
+    """Takes a molecular formula (string) and returns a dictionary of atom counts.
     Example: 'H2O' → {'H': 2, 'O': 1}"""
-      mol_dict={}
-  for i in split_before_uppercases(molecular_formula):
-    per, num =split_at_digit(i)
-    mol_dict[per] = num
-  return mol_dict
+    atom_count = {}
+    for i in split_before_each_uppercases(molecular_formula):
+      atom , count= split_at_first_digit(i)
+      atom_count[atom] = count
+    return atom_count
 
 def parse_chemical_reaction(reaction_equation):
     """Takes a reaction equation (string) and returns reactants and products as lists.  
