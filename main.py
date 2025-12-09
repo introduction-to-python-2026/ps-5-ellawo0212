604c0433f4a5475f2935af16946219db76cf3f6d
ellawo0212 <ellawo0212@gmail.com>
2025-12-09 22:31:51 +0200

    Refactor import statements for clarity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-# Add the import statements for functions from string_utils.py and equation_utils.py here
-from string_utils import parse_chemical_reaction, count_atoms_in_reaction
-from equation_utils import build_equations, my_solve
+from string_utils import parse_chemical_reaction,count_atoms_in_reaction
+from equation_utils import build_equations,my_solve
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
@@ -14,5 +13,3 @@ def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
     coefficients = my_solve(equations, coefficients) + [1]
 
     return coefficients # [1/3, 1, 2/3, 1]
-
-
